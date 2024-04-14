c0a1c0e0166625e91042632db75187491a85e737
lauram354 <l.murciac@uniandes.edu.co>
2024-04-14 00:20:24 -0500

    changes

--- main.ts
+++ main.ts
@@ -3,6 +3,13 @@ import { series} from './data.js';
 
 let seriesTbody: HTMLElement = document.getElementById('series')!;
 const averageSeasonsElm: HTMLElement = document.getElementById("average-seasons")!;
+let cardBody: HTMLElement = document.getElementById("cards")!;
+if (!cardBody) {
+    cardBody = document.createElement("div");
+    cardBody.id = "cards";
+    document.body.appendChild(cardBody);
+}
+
 
 renderSeriesInTable(series);
 
@@ -13,10 +20,19 @@ function renderSeriesInTable(series: Serie[]): void{
     series.forEach((serie) => {
         let trElement = document.createElement("tr");
         trElement.innerHTML = `<td>${serie.rank}</td>
-                                <td>${serie.name}</td>
+                                <td><a id="more-info-${serie.name}" href="#card-${serie.name}"> ${serie.name}</a></td>
                                 <td>${serie.channel}</td>
                                 <td>${serie.seasons}</td>`;
         seriesTbody.appendChild(trElement);
+
+        const moreInfoBtn: HTMLElement =  document.getElementById(`more-info-${serie.name}`)!;
+        moreInfoBtn.onclick = () => showMoreInfo(serie);
+
+        let cardElement = document.createElement("div");
+        cardElement.className = "card";
+        cardElement.id = `card-${serie.name}`;
+        cardElement.style.display = "none";
+        cardBody.appendChild(cardElement);
     });
 }
 
@@ -28,3 +44,29 @@ function getAverageSeasons(series: Serie[]): number {
     return averageSeasons;    
 
 }
+
+function showMoreInfo(serie: Serie): void{
+    const moreInfoCard: HTMLElement = document.getElementById(`card-${serie.name}`)!;
+    let imgElement = new Image();
+    imgElement.src = serie.image;
+    imgElement.alt = serie.name;
+    imgElement.width = 250;
+    let textElement = document.createElement("h3");
+    textElement.innerText = serie.name;
+    let descElement = document.createElement("p");
+    descElement.innerText = serie.description;
+    let linkElement = document.createElement("a");
+    linkElement.href = serie.link;
+    linkElement.target = "_blank";
+    linkElement.innerText = serie.link;
+
+    moreInfoCard.appendChild(imgElement);
+    moreInfoCard.appendChild(textElement);
+    moreInfoCard.appendChild(descElement);
+    moreInfoCard.appendChild(linkElement);
+    
+
+    moreInfoCard.style.display = "flex";
+
+}
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,27 @@
 import { series } from './data.js';
 var seriesTbody = document.getElementById('series');
 var averageSeasonsElm = document.getElementById("average-seasons");
+var cardBody = document.getElementById("cards");
+if (!cardBody) {
+    cardBody = document.createElement("div");
+    cardBody.id = "cards";
+    document.body.appendChild(cardBody);
+}
 renderSeriesInTable(series);
 averageSeasonsElm.innerHTML = "".concat(getAverageSeasons(series));
 function renderSeriesInTable(series) {
     console.log('Desplegando series');
     series.forEach(function (serie) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(serie.rank, "</td>\n                                <td>").concat(serie.name, "</td>\n                                <td>").concat(serie.channel, "</td>\n                                <td>").concat(serie.seasons, "</td>");
+        trElement.innerHTML = "<td>".concat(serie.rank, "</td>\n                                <td><a id=\"more-info-").concat(serie.name, "\" href=\"#card-").concat(serie.name, "\"> ").concat(serie.name, "</a></td>\n                                <td>").concat(serie.channel, "</td>\n                                <td>").concat(serie.seasons, "</td>");
         seriesTbody.appendChild(trElement);
+        var moreInfoBtn = document.getElementById("more-info-".concat(serie.name));
+        moreInfoBtn.onclick = function () { return showMoreInfo(serie); };
+        var cardElement = document.createElement("div");
+        cardElement.className = "card";
+        cardElement.id = "card-".concat(serie.name);
+        cardElement.style.display = "none";
+        cardBody.appendChild(cardElement);
     });
 }
 function getAverageSeasons(series) {
@@ -17,4 +30,24 @@ function getAverageSeasons(series) {
     series.forEach(function (serie) { return total = total + serie.seasons; });
     var averageSeasons = total / numSeries;
     return averageSeasons;
+}
+function showMoreInfo(serie) {
+    var moreInfoCard = document.getElementById("card-".concat(serie.name));
+    var imgElement = new Image();
+    imgElement.src = serie.image;
+    imgElement.alt = serie.name;
+    imgElement.width = 250;
+    var textElement = document.createElement("h3");
+    textElement.innerText = serie.name;
+    var descElement = document.createElement("p");
+    descElement.innerText = serie.description;
+    var linkElement = document.createElement("a");
+    linkElement.href = serie.link;
+    linkElement.target = "_blank";
+    linkElement.innerText = serie.link;
+    moreInfoCard.appendChild(imgElement);
+    moreInfoCard.appendChild(textElement);
+    moreInfoCard.appendChild(descElement);
+    moreInfoCard.appendChild(linkElement);
+    moreInfoCard.style.display = "flex";
 }
